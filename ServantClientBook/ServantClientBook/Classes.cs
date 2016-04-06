6066dbd6a12c01e3660e640d5b2bb7eb7223d97e
Katsutoshi Itoh <cut-sea@timedia.co.jp>
2016-04-06 22:47:38 +0900

    init csharp

---
+++ ServantClientBook/ServantClientBook/Classes.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+
+namespace ServantClientBook
+{
+    #region Address
+    [JsonObject("Address")]
+    public class Address
+    {
+        [JsonProperty("addressId")]
+        public int? addressId { get; set; }
+        [JsonProperty("postcode")]
+        public string postcode { get; set; }
+        [JsonProperty("prefecture")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Prefecture prefecture { get; set; }
+        [JsonProperty("address")]
+        public string address { get; set; }
+        [JsonProperty("building")]
+        public string building { get; set; }
+        [JsonProperty("tel")]
+        public string tel { get; set; }
+        [JsonProperty("fax")]
+        public string fax { get; set; }
+        [JsonProperty("email")]
+        public string email { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime createdAt { get; set; }
+        [JsonProperty("updatedAt")]
+        public DateTime updatedAt { get; set; }
+    }
+    #endregion
+    #region Author
+    [JsonObject("Author")]
+    public class Author
+    {
+        [JsonProperty("authorId")]
+        public int? authorId { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("gender")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Gender gender { get; set; }
+        [JsonProperty("birth")]
+        [JsonConverter(typeof(DayConverter))]
+        public DateTime birth { get; set; }
+        [JsonProperty("age")]
+        public int age { get; set; }
+        [JsonProperty("address")]
+        public Address address { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime createdAt { get; set; }
+        [JsonProperty("updatedAt")]
+        public DateTime updatedAt { get; set; }
+    }
+    #endregion
+    #region Publisher
+    [JsonObject("Publisher")]
+    public class Publisher
+    {
+        [JsonProperty("publisherId")]
+        public int? publisherId { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("companyType")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CompanyType companyType { get; set; }
+        [JsonProperty("address")]
+        public Address address { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime createdAt { get; set; }
+        [JsonProperty("updatedAt")]
+        public DateTime updatedAt { get; set; }
+    }
+    #endregion
+    #region Book
+    [JsonObject("Book")]
+    public class Book
+    {
+        [JsonProperty("bookId")]
+        public int? bookId { get; set; }
+        [JsonProperty("title")]
+        public string title { get; set; }
+        [JsonProperty("isbn")]
+        public string isbn { get; set; }
+        [JsonProperty("category")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Category category { get; set; }
+        [JsonProperty("description")]
+        public string description { get; set; }
+        [JsonProperty("publishedBy")]
+        public Publisher publishedBy { get; set; }
+        [JsonProperty("authors")]
+        public List<Author> authors { get; set; }
+        [JsonProperty("publishedAt")]
+        [JsonConverter(typeof(DayConverter))]
+        public DateTime publishedAt { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime createdAt { get; set; }
+        [JsonProperty("updatedAt")]
+        public DateTime updatedAt { get; set; }
+    }
+    #endregion
+}
---
+++ ServantClientBook/ServantClientBook/Enum.cs
@@ -0,0 +1,83 @@
+﻿namespace ServantClientBook
+{
+    #region Prefecture
+    public enum Prefecture
+    {
+        Hokkaido,
+        Aomori,
+        Iwate,
+        Miyagi,
+        Akita,
+        Yamagata,
+        Fukushima,
+        Ibaraki,
+        Tochigi,
+        Gunma,
+        Saitama,
+        Chiba,
+        Tokyo,
+        Kanagawa,
+        Nigata,
+        Toyama,
+        Ishikawa,
+        Fukui,
+        Yamanashi,
+        Nagano,
+        Gifu,
+        Shizuoka,
+        Aichi,
+        Mie,
+        Shiga,
+        Kyoto,
+        Osaka,
+        Hyogo,
+        Nara,
+        Wakayama,
+        Tottori,
+        Shimane,
+        Okayama,
+        Hiroshima,
+        Yamaguchi,
+        Tokushima,
+        Kagawa,
+        Ehime,
+        Kochi,
+        Fukuoka,
+        Saga,
+        Nagasaki,
+        Kumamoto,
+        Oita,
+        Miyazaki,
+        Kagoshima,
+        Okinawa
+    }
+    #endregion
+    #region Gender
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+    #endregion
+    #region CompanyType
+    public enum CompanyType
+    {
+        CO,
+        INC,
+        COM,
+        LLC,
+        COLTD
+    }
+    #endregion
+    #region Category
+    public enum Category
+    {
+        Science,
+        Computer,
+        Literate,
+        Textbook,
+        Comics,
+        Magazine
+    }
+    #endregion
+}
