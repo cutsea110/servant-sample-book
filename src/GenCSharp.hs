2531d1fa730f0fd41eacb35164f27e58a44411e1
cutsea110 <cutsea110@gmail.com>
2016-04-13 04:21:21 +0900

    update

--- src/GenCSharp.hs
+++ src/GenCSharp.hs
@@ -6,9 +6,13 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 module GenCSharp where
 
+import Prelude
+import Control.Arrow
 import Control.Lens
+import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
-import Data.Text
+import Data.Text (Text, pack, intercalate, toLower)
+import qualified Data.Text.IO as T
 import Data.Time (UTCTime)
 import Data.Time.Calendar (Day)
 import Data.Typeable
@@ -26,11 +30,22 @@ import API (api)
 
 data CSharp
 
+instance (HasForeignType CSharp t a, HasForeignType CSharp t' a)
+    => HasForeignType CSharp (t, t') a where
+        typeFor lang (Proxy :: Proxy (t, t')) p
+            = (typeFor lang (Proxy :: Proxy t) p, typeFor lang (Proxy :: Proxy t') p)
+
+instance Typeable a => HasForeignType CSharp TypeRep a where
+    typeFor _ _ p = typeRep p
+
 instance HasForeignType CSharp Text a => HasForeignType CSharp Text [a] where
     typeFor lang ftype (Proxy :: Proxy [t]) = "List<" <> typeFor lang ftype (Proxy :: Proxy t) <> ">"
 
 instance HasForeignType CSharp Text a => HasForeignType CSharp Text (Maybe a) where
     typeFor lang ftype (Proxy :: Proxy (Maybe t)) = "Nullable<" <> typeFor lang ftype (Proxy :: Proxy t) <> ">"
+
+instance {-# OVERLAPPING #-} HasForeignType CSharp Text () where
+    typeFor _ _ _ = "void"
 
 instance {-# OVERLAPPING #-} HasForeignType CSharp Text Int where
     typeFor _ _ _ = "int"
@@ -48,12 +63,12 @@ instance {-# OVERLAPPING #-} HasForeignType CSharp Text (Maybe String) where
     typeFor _ _ _ = "string"
 
 instance {-# OVERLAPPING #-} HasForeignType CSharp Text Day where
-    typeFor _ _ _ = "DateTime?"
+    typeFor _ _ _ = "DateTime"
 instance {-# OVERLAPPING #-} HasForeignType CSharp Text (Maybe Day) where
     typeFor _ _ _ = "DateTime?"
 
 instance {-# OVERLAPPING #-} HasForeignType CSharp Text UTCTime where
-    typeFor _ _ _ = "DateTime?"
+    typeFor _ _ _ = "DateTime"
 instance {-# OVERLAPPING #-} HasForeignType CSharp Text (Maybe UTCTime) where
     typeFor _ _ _ = "DateTime?"
 
@@ -63,8 +78,49 @@ instance {-# OVERLAPS #-} Typeable t => HasForeignType CSharp Text t where
 getEndpoints :: [Req Text]
 getEndpoints = listFromAPI (Proxy :: Proxy CSharp) (Proxy :: Proxy Text) api
 
-generateCSharpCs :: Req Text -> Text
-generateCSharpCs req = undefined
+getEndpoints' :: [Req (Text, TypeRep)]
+getEndpoints' = listFromAPI (Proxy :: Proxy CSharp) (Proxy :: Proxy (Text, TypeRep)) api
+
+data CSharpOption = CSharpOption { prefix :: Text
+                                 , jsonRequestBodyName ::Text
+                                 } deriving Show
+def :: CSharpOption
+def = CSharpOption { prefix = "_"
+                   , jsonRequestBodyName = "obj"
+                   }
+
+generateMethod :: CSharpOption -> Req Text -> Text
+generateMethod opt req = "\n" <> aDecl <> aBody <> decl <> body
+    where
+      aDecl = "public async " <> retTaskTyp <> " " <> fnameAsync <> "(" <> argsDecl <> ")"
+      aBody = " {\n" <> aStmts <> "}\n"
+
+      decl = "public " <> retTyp <> " " <> fname <> "(" <> argsDecl <> ")"
+      body = " {\n" <> stmts <> "}\n"
+
+      retTaskTyp = case retTyp of
+                     "void" -> "Task"
+                     t -> "Task<" <> t <> ">"
+      retTyp = fromJust $ req ^. reqReturnType
+      fnameAsync = fname <> "Async"
+      fname = req ^. reqFuncName . camelCaseL
+      argsDecl = intercalate "," $ map (\(t, n) -> t <> " " <> n) argsTyp_Nm
+
+      argsTyp_Nm :: [(Text, Text)]
+      argsTyp_Nm = map (id *** (prefix opt <>)) argsTypNm
+      argsTypNm, captures, rqBody, queryparams :: [(Text, Text)]
+      argsTypNm = captures ++ rqBody ++ queryparams
+      captures = map ((view argType &&& view argPath) . captureArg)
+                 . filter isCapture
+                 $ req ^. reqUrl . path
+      rqBody = maybe [] (pure . (id &&& const (jsonRequestBodyName opt)))
+               $ req ^. reqBody
+      queryparams = map ((view argType &&& unPathSegment . view argName)
+                        . view queryArgName)
+                    $ req ^. reqUrl . queryStr
+
+      aStmts = "    aStmts\n"
+      stmts = "    stmts\n"
 
 main :: IO ()
-main = putStrLn "developing"
+main = mapM_ (T.putStr . generateMethod def) getEndpoints
