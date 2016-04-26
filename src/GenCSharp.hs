1c0fd07bbbcd94b6124dc8583a1d77ef7fbe0901
cutsea110 <cutsea110@gmail.com>
2016-04-26 15:12:52 +0900

    update

--- src/GenCSharp.hs
+++ src/GenCSharp.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE GADTs #-}
 module GenCSharp where
 
+import Control.Arrow ((***), (&&&))
 import Control.Lens
+import Data.Aeson
 import qualified Data.HashMap.Lazy as M
 import Data.Swagger
 import Data.Text (Text)
@@ -21,6 +24,7 @@ pathitems = concatMap M.toList $ swagger^..paths
 
 enums :: [(Text, Schema)]
 enums = filter (has (paramSchema.enum_._Just) . snd) defs
+-- map (id *** ((^.paramSchema.type_) &&& (^.paramSchema.enum_._Just))) enums
 
 prims :: [(Text, Schema)]
 prims = filter (\def -> prim def && notEnum def) defs
@@ -31,4 +35,39 @@ prims = filter (\def -> prim def && notEnum def) defs
       primTypes = [ SwaggerString
                   , SwaggerInteger
                   , SwaggerNumber
-                  , SwaggerBoolean]
+                  , SwaggerBoolean
+                  ]
+-- map (id *** convert) prims
+
+models :: [(Text, Schema)]
+models = filter (\def -> notPrim def && notEnum def) defs
+    where
+      notEnum = (`notElem`enumTypes).fst
+      enumTypes = map fst enums
+      notPrim = (`notElem`primTypes).fst
+      primTypes = map fst prims
+
+data FieldType = FInteger
+               | FNumber
+               | FString
+               | FBool
+               | FDay
+               | FUTCTime
+               | FEnum Text [Value]
+               | FGeneral Text
+               | FList FieldType
+               | FNullable FieldType
+                 deriving Show
+
+convert :: (Text, Schema) -> FieldType
+convert (name, s)
+    = if null $ s^.paramSchema.enum_._Just
+      then case s^.type_ of
+             SwaggerString -> FString
+             SwaggerInteger -> FInteger
+             SwaggerNumber -> FNumber
+             SwaggerBoolean -> FBool
+             SwaggerArray -> error "convert don't support yet SwaggerArray"
+             SwaggerNull -> error "convert don't support yet SwaggerNull"
+             SwaggerObject -> FGeneral name
+      else FEnum name (s^.paramSchema.enum_._Just)
